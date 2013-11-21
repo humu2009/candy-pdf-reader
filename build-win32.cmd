d7b025fa5b5631834b44b2a25fe929621b00ae22
humu <humu2009@gmail.com>
2013-11-21 20:49:25 +0800

    initial update

---
+++ build-win32.cmd
@@ -0,0 +1,17 @@
+REM clear target folders
+del .\bin\nw-packge\*.* /q
+del .\bin\executable\win32\*.* /q
+
+REM generate the nw package for node-webkit
+.\tools\7z\7z -tzip a .\bin\nw-packge\viewer.nw .\pdf.js\*
+
+REM build the main executable file by merging nw.exe with the generated nw package
+copy /b ".\node-webkit\bin\win32\nw.exe"+".\bin\nw-packge\viewer.nw" .\bin\executable\win32\creader.exe
+
+REM also ship the required dependencies
+copy .\node-webkit\bin\win32\nw.pak .\bin\executable\win32\
+copy .\node-webkit\bin\win32\icudt.dll .\bin\executable\win32\
+copy .\node-webkit\bin\win32\ffmpegsumo.dll .\bin\executable\win32\
+copy .\node-webkit\bin\win32\credits.html .\bin\executable\win32\nw-credits.html
+
+pause
